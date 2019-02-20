46120da36c4342fc9142a6fa191e84a47f37d2ce
William Tsoi <william@williamtsoi.net>
2019-02-20 17:21:29 +1100

    linting issues

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,102 +1,102 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const providers = require('./templates/providers.json');
-const backends = require('./templates/backends.json');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const providers = require("./templates/providers.json");
+const backends = require("./templates/backends.json");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        'Welcome to the slick ' +
-          chalk.red('generator-terraform-environments') +
-          ' generator!'
+        "Welcome to the slick " +
+          chalk.red("generator-terraform-environments") +
+          " generator!"
       )
     );
 
     const prompts = [
       {
-        type: 'input',
-        name: 'environments',
+        type: "input",
+        name: "environments",
         message:
-          'What logical environments will you be running (separate multiple responses by comma)?',
-        default: 'staging,production',
+          "What logical environments will you be running (separate multiple responses by comma)?",
+        default: "staging,production",
         store: true
       },
       {
-        type: 'input',
-        name: 'components',
+        type: "input",
+        name: "components",
         message:
-          'What components will you be running (separate multiple responses by comma)?',
-        default: 'core,compute,data',
+          "What components will you be running (separate multiple responses by comma)?",
+        default: "core,compute,data",
         store: true
       },
       {
-        type: 'list',
-        name: 'backend',
+        type: "list",
+        name: "backend",
         message:
-          'What state backend will you be using? Full list of backends here: https://www.terraform.io/docs/backend/types/index.html',
+          "What state backend will you be using? Full list of backends here: https://www.terraform.io/docs/backend/types/index.html",
         choices: Object.keys(backends)
       },
       {
-        type: 'list',
-        name: 'provider',
+        type: "list",
+        name: "provider",
         message:
-          'What Terraform provider will you be using? Full list of providers here: https://www.terraform.io/docs/providers',
+          "What Terraform provider will you be using? Full list of providers here: https://www.terraform.io/docs/providers",
         choices: Object.keys(providers)
       },
       {
-        when: props => props.backend === 's3',
-        type: 'input',
-        name: 'backendBucketName',
-        message: 'Name of the S3 Bucket for remote state',
+        when: props => props.backend === "s3",
+        type: "input",
+        name: "backendBucketName",
+        message: "Name of the S3 Bucket for remote state",
         validate: input => input.length > 0
       },
       {
-        when: props => props.backend === 's3',
-        type: 'input',
-        name: 'backendBucketKeyPrefix',
-        message: 'The key prefix for the remote state files',
-        default: 'terraform-remote-state'
+        when: props => props.backend === "s3",
+        type: "input",
+        name: "backendBucketKeyPrefix",
+        message: "The key prefix for the remote state files",
+        default: "terraform-remote-state"
       },
       {
-        when: props => props.backend === 's3',
-        type: 'input',
-        name: 'backendBucketRegion',
-        message: 'The AWS region for the S3 Bucket',
-        default: 'ap-southeast-2'
+        when: props => props.backend === "s3",
+        type: "input",
+        name: "backendBucketRegion",
+        message: "The AWS region for the S3 Bucket",
+        default: "ap-southeast-2"
       },
       {
-        when: props => props.backend === 'consul',
-        type: 'input',
-        name: 'backendConsulAddress',
-        message: 'The address of the Consul agent',
-        default: 'localhost:8500',
+        when: props => props.backend === "consul",
+        type: "input",
+        name: "backendConsulAddress",
+        message: "The address of the Consul agent",
+        default: "localhost:8500",
         validate: input => input.length > 0
       },
       {
-        when: props => props.backend === 'consul',
-        type: 'input',
-        name: 'backendConsulPathPrefix',
-        message: 'The path prefix for the remote state',
-        default: 'terraform-remote-state'
+        when: props => props.backend === "consul",
+        type: "input",
+        name: "backendConsulPathPrefix",
+        message: "The path prefix for the remote state",
+        default: "terraform-remote-state"
       },
       {
-        when: props => props.backend === 'atlas',
-        type: 'input',
-        name: 'backendAtlasOrganisationName',
-        message: 'The name of your Terraform Enterprise Organisation',
-        default: 'myOrg',
+        when: props => props.backend === "atlas",
+        type: "input",
+        name: "backendAtlasOrganisationName",
+        message: "The name of your Terraform Enterprise Organisation",
+        default: "myOrg",
         validate: input => input.length > 0
       },
       {
-        when: props => props.backend === 'atlas',
-        type: 'input',
-        name: 'backendAtlasWorkspacePrefix',
-        message: 'Prefix for your Terraform Enterprise Workspaces',
-        default: 'myApp',
+        when: props => props.backend === "atlas",
+        type: "input",
+        name: "backendAtlasWorkspacePrefix",
+        message: "Prefix for your Terraform Enterprise Workspaces",
+        default: "myApp",
         validate: input => input.length > 0
       }
     ];
@@ -108,13 +108,13 @@ module.exports = class extends Generator {
   }
 
   writingEnvironments() {
-    var environments = this.props.environments.split(',');
-    var components = this.props.components.split(',');
+    var environments = this.props.environments.split(",");
+    var components = this.props.components.split(",");
     for (let component of components) {
       // Creates environment folders
       for (let environment of environments) {
         this.fs.copyTpl(
-          this.templatePath('environments/components/component.tf'),
+          this.templatePath("environments/components/component.tf"),
           this.destinationPath(
             `environments/${environment}/${component}/${component}.tf`
           ),
@@ -128,7 +128,8 @@ module.exports = class extends Generator {
             backendBucketRegion: this.props.backendBucketRegion,
             backendConsulAddress: this.props.backendConsulAddress,
             backendConsulPathPrefix: this.props.backendConsulPathPrefix,
-            backendAtlasOrganisationName: this.props.backendAtlasOrganisationName,
+            backendAtlasOrganisationName: this.props
+              .backendAtlasOrganisationName,
             backendAtlasWorkspacePrefix: this.props.backendAtlasWorkspacePrefix,
             environment: environment,
             component: component,
@@ -136,7 +137,7 @@ module.exports = class extends Generator {
           }
         );
         this.fs.copyTpl(
-          this.templatePath('environments/components/terraform.tfvars'),
+          this.templatePath("environments/components/terraform.tfvars"),
           this.destinationPath(
             `environments/${environment}/${component}/terraform.tfvars`
           ),
@@ -146,33 +147,40 @@ module.exports = class extends Generator {
           }
         );
         this.fs.copy(
-          this.templatePath('environments/components/input.tf'),
-          this.destinationPath(`environments/${environment}/${component}/input.tf`)
+          this.templatePath("environments/components/input.tf"),
+          this.destinationPath(
+            `environments/${environment}/${component}/input.tf`
+          )
         );
         this.fs.copy(
-          this.templatePath('environments/components/output.tf'),
-          this.destinationPath(`environments/${environment}/${component}/output.tf`)
+          this.templatePath("environments/components/output.tf"),
+          this.destinationPath(
+            `environments/${environment}/${component}/output.tf`
+          )
         );
-        this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
+        this.fs.copy(
+          this.templatePath("gitignore"),
+          this.destinationPath(".gitignore")
+        );
       }
     }
   }
 
   writingModules() {
-    var components = this.props.components.split(',');
+    var components = this.props.components.split(",");
     for (let component of components) {
       // Creates module folders
       this.fs.copy(
-        this.templatePath('modules/input.tf'),
+        this.templatePath("modules/input.tf"),
         this.destinationPath(`modules/${component}/input.tf`)
       );
       this.fs.copyTpl(
-        this.templatePath('modules/module.tf'),
+        this.templatePath("modules/module.tf"),
         this.destinationPath(`modules/${component}/${component}.tf`),
         { component: component }
       );
       this.fs.copy(
-        this.templatePath('modules/output.tf'),
+        this.templatePath("modules/output.tf"),
         this.destinationPath(`modules/${component}/output.tf`)
       );
     }
--- helpers/backendsTestData.js
+++ helpers/backendsTestData.js
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const helpers = require('yeoman-test');
+const path = require("path");
+const helpers = require("yeoman-test");
 
 exports.s3BackendTestData = () => {
-  return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
-    environments: '1,2,3',
-    components: 'a,b,c',
-    backend: 's3',
-    provider: 'aws',
-    backendBucketName: 'myBucket',
-    backendBucketKeyPrefix: 'terraform-remote-state',
-    backendBucketRegion: 'ap-southeast-2'
+  return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+    environments: "1,2,3",
+    components: "a,b,c",
+    backend: "s3",
+    provider: "aws",
+    backendBucketName: "myBucket",
+    backendBucketKeyPrefix: "terraform-remote-state",
+    backendBucketRegion: "ap-southeast-2"
   });
 };
 
 exports.consulBackendTestData = () => {
-  return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
-    environments: '1,2,3',
-    components: 'a,b,c',
-    backend: 'consul',
-    provider: 'aws',
-    backendConsulAddress: 'consul',
-    backendConsulPathPrefix: 'terraform-remote-state'
+  return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+    environments: "1,2,3",
+    components: "a,b,c",
+    backend: "consul",
+    provider: "aws",
+    backendConsulAddress: "consul",
+    backendConsulPathPrefix: "terraform-remote-state"
   });
 };
 
 exports.atlasBackendTestData = () => {
-  return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
-    environments: '1,2,3',
-    components: 'a,b,c',
-    backend: 'atlas',
-    provider: 'aws',
-    backendAtlasOrganisationName: 'myOrg',
-    backendAtlasWorkspacePrefix: 'myApp'
+  return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+    environments: "1,2,3",
+    components: "a,b,c",
+    backend: "atlas",
+    provider: "aws",
+    backendAtlasOrganisationName: "myOrg",
+    backendAtlasWorkspacePrefix: "myApp"
   });
 };
